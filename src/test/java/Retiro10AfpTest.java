89181c75a4ae503298d54fd700331569cb392cd2
tnahuelpan <tabigail.nh@gmail.com>
2021-02-08 22:36:40 -0300

    Se añaden el resto de Tests

--- src/test/java/Retiro10AfpTest.java
+++ src/test/java/Retiro10AfpTest.java
@@ -30,7 +30,7 @@ public class Retiro10AfpTest {
     }
 
     @Test
-    public void testCasoNegocio1() throws InterruptedException {
+    public void testCasoNegocio1A() throws InterruptedException {
         driver.findElement(By.id("inputSueldo")).sendKeys("1499999");
         driver.findElement(By.id("inputAhorro")).sendKeys("1020109");
         driver.findElement(By.id("button")).click();
@@ -42,7 +42,55 @@ public class Retiro10AfpTest {
     }
 
     @Test
-    public void testCasoNegocio2() throws InterruptedException {
+    public void testCasoNegocio1B() throws InterruptedException {
+        driver.findElement(By.id("inputSueldo")).sendKeys("1499999");
+        driver.findElement(By.id("inputAhorro")).sendKeys("1020109");
+        driver.findElement(By.id("button")).click();
+
+        Thread.sleep(5000);
+
+        String resultado = "0";
+        assertEquals(resultado, driver.findElement(By.id("saldo")).getText());
+    }
+
+    @Test
+    public void testCasoNegocio1C() throws InterruptedException {
+        driver.findElement(By.id("inputSueldo")).sendKeys("1499999");
+        driver.findElement(By.id("inputAhorro")).sendKeys("1020109");
+        driver.findElement(By.id("button")).click();
+
+        Thread.sleep(5000);
+
+        String resultado = "0";
+        assertEquals(resultado, driver.findElement(By.id("impuesto")).getText());
+    }
+
+    @Test
+    public void testCasoNegocio2A() throws InterruptedException {
+        driver.findElement(By.id("inputSueldo")).sendKeys("1500000");
+        driver.findElement(By.id("inputAhorro")).sendKeys("1020110");
+        driver.findElement(By.id("button")).click();
+
+        Thread.sleep(5000);
+
+        String resultado = "1020110";
+        assertEquals(resultado, driver.findElement(By.id("dxc")).getText());
+    }
+
+    @Test
+    public void testCasoNegocio2B() throws InterruptedException {
+        driver.findElement(By.id("inputSueldo")).sendKeys("1500000");
+        driver.findElement(By.id("inputAhorro")).sendKeys("1020110");
+        driver.findElement(By.id("button")).click();
+
+        Thread.sleep(5000);
+
+        String resultado = "0";
+        assertEquals(resultado, driver.findElement(By.id("saldo")).getText());
+    }
+
+    @Test
+    public void testCasoNegocio2C() throws InterruptedException {
         driver.findElement(By.id("inputSueldo")).sendKeys("1500000");
         driver.findElement(By.id("inputAhorro")).sendKeys("1020110");
         driver.findElement(By.id("button")).click();
@@ -54,7 +102,7 @@ public class Retiro10AfpTest {
     }
 
     @Test
-    public void testCasoNegocio3() throws InterruptedException {
+    public void testCasoNegocio3A() throws InterruptedException {
         driver.findElement(By.id("inputSueldo")).sendKeys("2500000");
         driver.findElement(By.id("inputAhorro")).sendKeys("1020111");
         driver.findElement(By.id("button")).click();
@@ -66,7 +114,43 @@ public class Retiro10AfpTest {
     }
 
     @Test
-    public void testCasoNegocio4() throws InterruptedException {
+    public void testCasoNegocio3B() throws InterruptedException {
+        driver.findElement(By.id("inputSueldo")).sendKeys("2500000");
+        driver.findElement(By.id("inputAhorro")).sendKeys("1020111");
+        driver.findElement(By.id("button")).click();
+
+        Thread.sleep(5000);
+
+        String resultado = "1";
+        assertEquals(resultado, driver.findElement(By.id("saldo")).getText());
+    }
+
+    @Test
+    public void testCasoNegocio3C() throws InterruptedException {
+        driver.findElement(By.id("inputSueldo")).sendKeys("2500000");
+        driver.findElement(By.id("inputAhorro")).sendKeys("1020111");
+        driver.findElement(By.id("button")).click();
+
+        Thread.sleep(5000);
+
+        String resultado = "364086";
+        assertEquals(resultado, driver.findElement(By.id("impuesto")).getText());
+    }
+
+    @Test
+    public void testCasoNegocio4A() throws InterruptedException {
+        driver.findElement(By.id("inputSueldo")).sendKeys("3000000");
+        driver.findElement(By.id("inputAhorro")).sendKeys("10201100");
+        driver.findElement(By.id("button")).click();
+
+        Thread.sleep(5000);
+
+        String resultado = "1020110";
+        assertEquals(resultado, driver.findElement(By.id("dxc")).getText());
+    }
+
+    @Test
+    public void testCasoNegocio4B() throws InterruptedException {
         driver.findElement(By.id("inputSueldo")).sendKeys("3000000");
         driver.findElement(By.id("inputAhorro")).sendKeys("10201100");
         driver.findElement(By.id("button")).click();
@@ -78,7 +162,19 @@ public class Retiro10AfpTest {
     }
 
     @Test
-    public void testCasoNegocio5() throws InterruptedException {
+    public void testCasoNegocio4C() throws InterruptedException {
+        driver.findElement(By.id("inputSueldo")).sendKeys("3000000");
+        driver.findElement(By.id("inputAhorro")).sendKeys("10201100");
+        driver.findElement(By.id("button")).click();
+
+        Thread.sleep(5000);
+
+        String resultado = "612396";
+        assertEquals(resultado, driver.findElement(By.id("impuesto")).getText());
+    }
+
+    @Test
+    public void testCasoNegocio5A() throws InterruptedException {
         driver.findElement(By.id("inputSueldo")).sendKeys("4000000");
         driver.findElement(By.id("inputAhorro")).sendKeys("11201100");
         driver.findElement(By.id("button")).click();
@@ -90,7 +186,43 @@ public class Retiro10AfpTest {
     }
 
     @Test
-    public void testCasoNegocio6() throws InterruptedException {
+    public void testCasoNegocio5B() throws InterruptedException {
+        driver.findElement(By.id("inputSueldo")).sendKeys("4000000");
+        driver.findElement(By.id("inputAhorro")).sendKeys("11201100");
+        driver.findElement(By.id("button")).click();
+
+        Thread.sleep(5000);
+
+        String resultado = "10080990";
+        assertEquals(resultado, driver.findElement(By.id("saldo")).getText());
+    }
+
+    @Test
+    public void testCasoNegocio5C() throws InterruptedException {
+        driver.findElement(By.id("inputSueldo")).sendKeys("4000000");
+        driver.findElement(By.id("inputAhorro")).sendKeys("11201100");
+        driver.findElement(By.id("button")).click();
+
+        Thread.sleep(5000);
+
+        String resultado = "980357";
+        assertEquals(resultado, driver.findElement(By.id("impuesto")).getText());
+    }
+
+    @Test
+    public void testCasoNegocio6A() throws InterruptedException {
+        driver.findElement(By.id("inputSueldo")).sendKeys("5000000");
+        driver.findElement(By.id("inputAhorro")).sendKeys("43719000");
+        driver.findElement(By.id("button")).click();
+
+        Thread.sleep(5000);
+
+        String resultado = "4371900";
+        assertEquals(resultado, driver.findElement(By.id("dxc")).getText());
+    }
+
+    @Test
+    public void testCasoNegocio6B() throws InterruptedException {
         driver.findElement(By.id("inputSueldo")).sendKeys("5000000");
         driver.findElement(By.id("inputAhorro")).sendKeys("43719000");
         driver.findElement(By.id("button")).click();
@@ -102,7 +234,43 @@ public class Retiro10AfpTest {
     }
 
     @Test
-    public void testCasoNegocio7() throws InterruptedException {
+    public void testCasoNegocio6C() throws InterruptedException {
+        driver.findElement(By.id("inputSueldo")).sendKeys("5000000");
+        driver.findElement(By.id("inputAhorro")).sendKeys("43719000");
+        driver.findElement(By.id("button")).click();
+
+        Thread.sleep(5000);
+
+        String resultado = "1294052";
+        assertEquals(resultado, driver.findElement(By.id("impuesto")).getText());
+    }
+
+    @Test
+    public void testCasoNegocio7A() throws InterruptedException {
+        driver.findElement(By.id("inputSueldo")).sendKeys("6000000");
+        driver.findElement(By.id("inputAhorro")).sendKeys("43719001");
+        driver.findElement(By.id("button")).click();
+
+        Thread.sleep(5000);
+
+        String resultado = "4371900";
+        assertEquals(resultado, driver.findElement(By.id("dxc")).getText());
+    }
+
+    @Test
+    public void testCasoNegocio7B() throws InterruptedException {
+        driver.findElement(By.id("inputSueldo")).sendKeys("6000000");
+        driver.findElement(By.id("inputAhorro")).sendKeys("43719001");
+        driver.findElement(By.id("button")).click();
+
+        Thread.sleep(5000);
+
+        String resultado = "39347101";
+        assertEquals(resultado, driver.findElement(By.id("saldo")).getText());
+    }
+
+    @Test
+    public void testCasoNegocio7C() throws InterruptedException {
         driver.findElement(By.id("inputSueldo")).sendKeys("6000000");
         driver.findElement(By.id("inputAhorro")).sendKeys("43719001");
         driver.findElement(By.id("button")).click();
